15dd8c4d98e8b21e681b8a8df44da66bd616158f
Anderson <@1001ander>
2026-01-31 16:06:00 -0400

    implementar mappers

--- app/src/main/java/edu/ucne/RegistroDeEstudiantes/data/penalidades/mapper/TipoPenalidadMappers.kt
+++ app/src/main/java/edu/ucne/RegistroDeEstudiantes/data/penalidades/mapper/TipoPenalidadMappers.kt
@@ -1,2 +1,23 @@
 package edu.ucne.RegistroDeEstudiantes.data.penalidades.mapper
 
+import edu.ucne.RegistroDeEstudiantes.data.penalidades.local.TipoPenalidadEntity
+import edu.ucne.RegistroDeEstudiantes.domain.penalidades.model.TipoPenalidad
+
+fun TipoPenalidadEntity.toDomain(): TipoPenalidad {
+    return TipoPenalidad(
+        tipoId = tipoId ?: 0,
+        nombre = nombre,
+        descripcion = descripcion,
+        puntosDescuento = puntosDescuento
+    )
+}
+
+fun TipoPenalidad.toEntity(): TipoPenalidadEntity {
+    return TipoPenalidadEntity(
+        tipoId = tipoId.takeIf { it > 0 },
+        nombre = nombre,
+        descripcion = descripcion,
+        puntosDescuento = puntosDescuento
+    )
+}
+
